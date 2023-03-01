dcb0586e10c30624ba8d2be941c2a25f04ea739f
joercontrib <126700632+joercontrib@users.noreply.github.com>
2023-03-01 20:21:56 +0000

    Add include guard

--- data/colours.h
+++ data/colours.h
@@ -1,6 +1,8 @@
 /* Copyright (C) 2023 Analog Devices, Inc. */
 /* Some colours */
 
+#ifndef COLOURS_H
+
 #define RED 1
 #define ORANGE 2
 #define YELLOW 3
@@ -8,3 +10,5 @@
 #define BLUE 5
 #define INDIGO 6
 #define VIOLET 7
+
+#endif 
